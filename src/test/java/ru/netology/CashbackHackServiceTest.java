74aa5039e6160f892e1577f33a2196e5ef4fa162
Your Name <you@example.com>
2023-05-05 14:17:44 +0500

    junit4 commit

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,30 +1,23 @@
 package ru.netology;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
-    public void testRemainBoundaryValuesOne() {
-        org.testng.Assert.assertThrows(RuntimeException.class, () -> {
-            service.remain(-1);
-        });
-    }
 
-
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldCorrectAmount() {
         assertEquals(service.remain(2800), 200);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldAmountZero() {
         assertEquals(service.remain(0), 1000);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldBuyExtraIfTheAmount1000() {
         assertEquals(service.remain(1000), 0);
     }
